013e267054f8f1fe90e27c35243db35b3a90aa5e
poynting-dev <dixitpriyanshu23@gmail.com>
2021-09-06 22:38:29 +0530

    Encrypting password using AES-256

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
@@ -11,12 +13,15 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
       email: String,
       password: String
-};
+});
 
-const User = mongoose.model("User", userSchema);
+const secret = "Youcannotdecodeit,evenifyouwant";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
   
 // const article = [
 //       {
